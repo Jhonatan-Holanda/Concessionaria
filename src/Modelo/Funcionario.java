2ce0ff6eed90112eea43bb41d626785d4cd47292
Jhonatan_Holanda <jhonatan_holanda@hotmail.com>
2018-03-05 13:30:09 -0300

    Iniciação do projeto como conexao e os arquivos necessarios para o login

---
+++ src/Modelo/Funcionario.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Jhonatan Holanda
+ */
+public class Funcionario {
+    private int cod;
+    private String nome;
+    private String email;
+    private String cpf;
+    private String telefone;
+    private String rg;
+    private int numeropis;
+    private String login;
+    private String senha;
+    private int patente;
+
+    public int getCod() {
+        return cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public int getNumeropis() {
+        return numeropis;
+    }
+
+    public void setNumeropis(int numeropis) {
+        this.numeropis = numeropis;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public int getPatente() {
+        return patente;
+    }
+
+    public void setPatente(int patente) {
+        this.patente = patente;
+    }
+}
