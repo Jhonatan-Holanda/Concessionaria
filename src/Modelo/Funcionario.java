2310f13827969bf3912570a75faaeb50a5f5784c
MatheusMartins <MatheusMartins@LAPTOP-GB59GKF5.mshome.net>
2018-03-06 13:20:04 -0300

    Cadastrar Funncionario

---
+++ src/Modelo/Funcionario.java
@@ -0,0 +1,70 @@
+package Modelo;
+
+
+public class Funcionario {
+    private String cpf;
+    private String nomecompleto;
+    private int telefone;
+    private String email;
+    private int RG;
+    private int numerodopis;
+    private String endereco;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNomecompleto() {
+        return nomecompleto;
+    }
+
+    public void setNomecompleto(String nomecompleto) {
+        this.nomecompleto = nomecompleto;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getRG() {
+        return RG;
+    }
+
+    public void setRG(int RG) {
+        this.RG = RG;
+    }
+
+    public int getNumerodopis() {
+        return numerodopis;
+    }
+
+    public void setNumerodopis(int numerodopis) {
+        this.numerodopis = numerodopis;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    
+   
+}
