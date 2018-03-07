c7e5ac51cb6b03cf56ab1b82e35ee3a80f40650f
Rudinilly <Rudinilly@Rudinilly-PC>
2018-03-06 23:48:15 -0300

    Atualização: Cadastrar Promoções

---
+++ src/Visao/Cadastrar/CadastrarPromocao.java
@@ -0,0 +1,189 @@
+package Visao.Cadastrar;
+
+import DAO.Conexao;
+import DAO.PromocaoDAO;
+import Modelo.Promocao;
+import java.sql.Connection;
+import java.text.SimpleDateFormat;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Rudinilly
+ */
+public class CadastrarPromocao extends javax.swing.JFrame {
+
+    public CadastrarPromocao() {
+        initComponents();
+        setLocationRelativeTo(this);
+        setResizable(false);
+        setTitle("Concessionaria");
+    }
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        jComboBox1 = new javax.swing.JComboBox<>();
+        jLabel3 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jDateChooser1 = new com.toedter.calendar.JDateChooser();
+        jLabel4 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+
+        jPanel1.setLayout(null);
+
+        jLabel1.setFont(new java.awt.Font("Arial Black", 0, 36)); // NOI18N
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Cadastrar Promoção");
+        jPanel1.add(jLabel1);
+        jLabel1.setBounds(0, 0, 590, 80);
+
+        jPanel2.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        jPanel2.setLayout(null);
+
+        jLabel2.setText("Veículo:");
+        jPanel2.add(jLabel2);
+        jLabel2.setBounds(50, 20, 80, 30);
+
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "dasdas" }));
+        jComboBox1.setToolTipText("");
+        jPanel2.add(jComboBox1);
+        jComboBox1.setBounds(130, 20, 300, 30);
+
+        jLabel3.setText("Deconto:");
+        jPanel2.add(jLabel3);
+        jLabel3.setBounds(50, 70, 80, 30);
+        jPanel2.add(jTextField1);
+        jTextField1.setBounds(130, 70, 40, 30);
+        jPanel2.add(jDateChooser1);
+        jDateChooser1.setBounds(130, 130, 140, 30);
+
+        jLabel4.setText("Data Termino:");
+        jPanel2.add(jLabel4);
+        jLabel4.setBounds(50, 130, 80, 30);
+
+        jPanel1.add(jPanel2);
+        jPanel2.setBounds(0, 90, 590, 180);
+
+        jButton1.setText("Cancelar");
+        jPanel1.add(jButton1);
+        jButton1.setBounds(430, 290, 100, 40);
+
+        jButton2.setText("Cadastrar");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton2);
+        jButton2.setBounds(60, 290, 100, 40);
+
+        jButton3.setText("Limpar");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton3);
+        jButton3.setBounds(250, 290, 100, 40);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 590, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 350, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+    jTextField1.setText("");
+    jComboBox1.setSelectedIndex(0);
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    String veiculo = jComboBox1.getSelectedItem().toString();
+    String desconto = jTextField1.getText();
+        if (veiculo.equals("")||desconto.equals("")||jDateChooser1.getDate().equals("")) {
+            JOptionPane.showMessageDialog(null,"Todos os campos devem ser preenchidos","Concessionaria",JOptionPane.WARNING_MESSAGE);
+        }else{
+            String data_devolucao = new SimpleDateFormat("dd/MM/yyyy").format(jDateChooser1.getDate());
+            int des = Integer.parseInt(desconto);
+            Connection con = Conexao.AbrirConexao();
+            PromocaoDAO sql = new PromocaoDAO(con);
+            Promocao p = new Promocao();
+            p.setVeiculo(veiculo);
+            p.setPercentual(des);
+            p.setData_expiracao(data_devolucao);
+            
+            sql.Inserir_Promocao(p);
+             jTextField1.setText("");
+            jComboBox1.setSelectedIndex(0);
+            Conexao.FecharConexao(con);
+        }
+            
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(CadastrarPromocao.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(CadastrarPromocao.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(CadastrarPromocao.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(CadastrarPromocao.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new CadastrarPromocao().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JComboBox<String> jComboBox1;
+    private com.toedter.calendar.JDateChooser jDateChooser1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JTextField jTextField1;
+    // End of variables declaration//GEN-END:variables
+}
